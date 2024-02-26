3b373cd37b836b96d3c2148f980d7d9b5641b93c
Julia Voychenko <jvoychenko@gmail.com>
2024-02-26 21:58:08 +0300

    add template

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { ScoreContainer, ScoreCell, Timer, Bank } from "./components.styled";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const cellValues = [2, 4, 16, 32, 64, 128];
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <ScoreContainer>
+        {cellValues.reverse().map((value) => (
+          <ScoreCell $isActive={false}>{value}</ScoreCell>
+        ))}
+      </ScoreContainer>
+      <Timer>Timer</Timer>
+      <Bank>Bank</Bank>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components.styled.ts
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+
+export const ScoreContainer = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  margin-bottom: 20px;
+`;
+
+export const ScoreCell = styled.div<{ $isActive: boolean }>`
+  font-size: 25px;
+  width: 150px;
+  display: flex;
+  justify-content: space-around;
+  background-color: ${({ $isActive }) =>
+    $isActive ? "#007FFF" : "rgba(152, 152, 152, 0.23)"};
+
+  border-left: 3px solid rgba(255, 255, 255, 0.87);
+  border-right: 3px solid rgba(255, 255, 255, 0.87);
+
+  &:first-child {
+    border-top: 3px solid rgba(255, 255, 255, 0.87);
+    border-radius: 20px 20px 0 0;
+  }
+
+  &:last-child {
+    border-bottom: 3px solid rgba(255, 255, 255, 0.87);
+    border-radius: 0 0 20px 20px;
+  }xs
+`;
+
+export const Timer = styled.div`
+  font-size: 30px;
+  display: flex;
+  justify-content: space-around;
+`;
+
+export const Bank = styled.div`
+  font-size: 50px;
+  display: flex;
+  justify-content: space-around;
+`;
