bef7f7407d2f38bfe295d97e355a6ee582529444
Julia Voychenko <jvoychenko@gmail.com>
2024-02-28 19:29:50 +0300

    fix timer stopper

--- src/CountdownTimer.tsx
+++ src/CountdownTimer.tsx
@@ -1,30 +1,35 @@
 import { useEffect, useState } from "react";
 import { Timer } from "./components.styled";
-import { useKeyPress, useTimer } from "./hooks";
+import { useKeyPress } from "./hooks";
 import { Key } from "./types";
 import { formatSeconds, getReturnValues } from "./utils";
 
 //три минуты по шестьдесят секуед на тысячу милисекунд
-const MINUTES_IN_MS = 3 * 60 * 1000;
+const MINUTES_IN_MS = 5 * 1000;
+const dateTimeAfterThreeMinutes = new Date().getTime() + MINUTES_IN_MS;
 const NOW = new Date().getTime();
+let interval;
 
 export const CountdownTimer = () => {
-  const dateTimeAfterThreeMinutes = NOW + MINUTES_IN_MS;
-
   const [timer, setTimer] = useState<number>(dateTimeAfterThreeMinutes - NOW);
 
+  const countDownTimer = new Date(dateTimeAfterThreeMinutes).getTime();
   const [minutes, seconds] = getReturnValues(timer);
   const formattedSeconds = formatSeconds(seconds);
 
-  const countDownTimer = new Date(dateTimeAfterThreeMinutes).getTime();
-
   useKeyPress(() => {
-    const interval = setInterval(() => {
+    interval = setInterval(() => {
       setTimer(countDownTimer - new Date().getTime());
     }, 1000);
-
-    return () => clearInterval(interval);
   }, Key.S);
+
+  useEffect(() => {
+    console.log(timer);
+    if (minutes === 0 && seconds === 0) {
+      //   console.log(timer);
+      clearInterval(interval);
+    }
+  }, [timer]);
 
   return (
     <Timer>
